278b4eaed0e4a0d1c213b3b9b1b4697c2ea63869
ardelagi <ardetmg031@gmail.com>
2025-08-29 05:42:23 +0700

    Update ready.js

--- src/events/Client/ready.js
+++ src/events/Client/ready.js
@@ -1,7 +1,7 @@
 const { prefix } = require("../../config.js");
 
 module.exports ={
-name: "ready",
+name: "clientReady",
 run: async (client) => {
     client.logger.log(`${client.user.username} online!`, "ready");
     client.logger.log(`Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`, "ready");
